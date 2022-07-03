ac7ccf84f4678a9b93ccd8479eb5cd291748553b
ukami <ukami@interserve.co.jp>
2022-07-04 00:24:20 +0900

    1

--- Src_Swagger/WebApplication1/WebApplication1/Program.cs
+++ Src_Swagger/WebApplication1/WebApplication1/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.OpenApi.Models;
+using System.Diagnostics;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", 
+        new OpenApiInfo { 
+            Title = Assembly.GetEntryAssembly().GetName().Name, 
+            Version = "v" + FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion
+    });
+});
 
 var app = builder.Build();
 
