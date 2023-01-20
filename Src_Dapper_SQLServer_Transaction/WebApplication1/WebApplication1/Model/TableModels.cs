58769c364de37c394950a4dce5df7c83baa947b7
ukami <ukami@interserve.co.jp>
2023-01-20 16:43:34 +0900

    1

---
+++ Src_Dapper_SQLServer_Transaction/WebApplication1/WebApplication1/Model/TableModels.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WebApplication1.Model
+{
+    public class TableA
+    {
+        public long Id { get; set; }
+        public string Code { get; set; }
+        public string? ValueString { get; set; }
+        public DateTime? ValueDate { get; set; }
+    }
+}
