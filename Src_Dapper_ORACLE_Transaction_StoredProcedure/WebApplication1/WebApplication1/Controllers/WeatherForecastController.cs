69c229d4b8cc271bb0beb8a88c3bfc08246df579
ukami <ukami@interserve.co.jp>
2023-01-29 18:21:21 +0900

    1

---
+++ Src_Dapper_ORACLE_Transaction_StoredProcedure/WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+using WebApplication1.SQL;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost(Name = "PostWeatherForecast")]
+        public IActionResult PostWeatherForecast(TABLE1 insertTABLE1)
+        {
+            SQL_TABLE1.Transaction1(insertTABLE1);
+
+            return Ok();
+        }
+    }
+}
---
+++ Src_Dapper_ORACLE_Transaction_StoredProcedure/WebApplication1/WebApplication1/SQL/SQL_TABLE1.cs
@@ -0,0 +1,57 @@
+﻿using System.Data;
+using WebApplication1.Model;
+using WebApplication1.Shared;
+using Dapper;
+using Microsoft.Extensions.Logging;
+using Oracle.ManagedDataAccess.Client;
+
+namespace WebApplication1.SQL
+{
+    public class SQL_TABLE1
+    {
+        public static void Transaction1(TABLE1 insertTABLE1)
+        {
+            using (var conn = new OracleConnection(SharedData.ConnectionString))
+            {
+                conn.Open();
+                using (var tran = conn.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    try
+                    {
+                        var table1List = Select(conn);
+
+                        Insert(conn, tran, insertTABLE1);
+
+                        var table1List2 = Select(conn);
+
+                        tran.Commit();
+                    }
+                    catch
+                    {
+                        tran.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<TABLE1> Select(OracleConnection conn)
+        {
+            return conn.Query<TABLE1>("SP_TABLE1_SELECT", null, commandType: CommandType.StoredProcedure);
+        }
+
+        public static void Insert(OracleConnection conn, OracleTransaction tran, TABLE1 insertTABLE1)
+        {
+            var sqlParam = new
+            {
+                id = insertTABLE1.ID,
+                code = insertTABLE1.CODE,
+                value_string = insertTABLE1.VALUE_STRING,
+                value_date = insertTABLE1.VALUE_DATE
+            };
+
+            conn.Execute("SP_TABLE1_INSERT", sqlParam, tran, commandType: CommandType.StoredProcedure);
+        }
+    }
+
+}
